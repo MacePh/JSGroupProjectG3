5c55599e9fe7cd6289bf04c7ee6015ced76f2f8a
MacePh <115651516+MacePh@users.noreply.github.com>
2022-11-16 11:30:00 -0500

    got java working for adding buttons to list

--- todo.js
+++ todo.js
@@ -1,11 +1,28 @@
-const parentUL = document.querySelector('ul');
-const sumbit = document.querySelector('button');
 
-submit.addEventListener('click', function(event) {
-	let newLI = document.createElement('li');
-    newLI.innerHTML=document.getElementById('newToDo').value;
-    parentUL.appendChild(newLI);
-    document.getElementById("newToDo").value = ""});
-    
-
-
+            const parentUL = document.getElementById('tdUl');
+            // console.log(parentUL);
+            // const myInput = document.getElementById('myInput').value;
+            function submit() {
+                let newLI = document.createElement('li');
+                let newBtn = document.createElement('button');
+                newBtn.innerHTML=(myInput).value;
+                
+                newBtn.className="itemButton btn-lg";
+                newLI.appendChild(newBtn);
+               parentUL.appendChild(newLI);
+               document.getElementById("myInput").value = ""};
+            console.log(newLI);
+               
+            //     // newLI.innerHTML=newBtn.e;
+                
+            //     
+            //     console.log('newBtn')
+            //     var ul = document.getElementById("list");
+// var li = document.createElement("li");
+// li.appendChild(document.createTextNode("Four"));
+// var button = document.createElement("button");
+// button.innerHTML = "asdasd";
+// li.appendChild(button);
+// li.setAttribute("id","element4");
+// ul.appendChild(li);
+// alert(li.id);//
