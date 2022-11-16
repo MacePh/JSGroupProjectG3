beb3451db61b8cc448f9ff0677cf747ce7ff1fd6
MacePh <115651516+MacePh@users.noreply.github.com>
2022-11-16 11:37:33 -0500

    update to html

--- todo.js
+++ todo.js
@@ -1,28 +1,16 @@
 
+            //Calls the UL into memory
             const parentUL = document.getElementById('tdUl');
-            // console.log(parentUL);
-            // const myInput = document.getElementById('myInput').value;
+
+            //Function executed on click of add button
             function submit() {
-                let newLI = document.createElement('li');
-                let newBtn = document.createElement('button');
-                newBtn.innerHTML=(myInput).value;
-                
-                newBtn.className="itemButton btn-lg";
-                newLI.appendChild(newBtn);
-               parentUL.appendChild(newLI);
-               document.getElementById("myInput").value = ""};
+                let newLI = document.createElement('li');//creates new list item
+                let newBtn = document.createElement('button');//creates a button to go into new list item
+                newBtn.innerHTML=(myInput).value;//pulls input form into the button text
+                newBtn.className="itemButton btn-lg";//gives button appropriate class names
+                newLI.appendChild(newBtn);//puts newBtn into the list item
+               parentUL.appendChild(newLI);//inserts complete button into the origninal UL
+               document.getElementById("myInput").value = ""};//clears the input form
             console.log(newLI);
                
-            //     // newLI.innerHTML=newBtn.e;
-                
-            //     
-            //     console.log('newBtn')
-            //     var ul = document.getElementById("list");
-// var li = document.createElement("li");
-// li.appendChild(document.createTextNode("Four"));
-// var button = document.createElement("button");
-// button.innerHTML = "asdasd";
-// li.appendChild(button);
-// li.setAttribute("id","element4");
-// ul.appendChild(li);
-// alert(li.id);//
+    
