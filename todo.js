d0c8d571b662962167dc9d64b03e18693b48e93e
MacePh <115651516+MacePh@users.noreply.github.com>
2022-11-17 14:34:47 -0500

    update that works!

--- todo.js
+++ todo.js
@@ -1,67 +1,182 @@
-let form = document.getElementById('addForm');
-let itemList = document.getElementById('items');
-let filter = document.getElementById('filter');
+const deleteB = document.querySelector('span.deleteBtn')
+const parentUL = document.getElementById('tdUl');
 
-// Form submit event
-form.addEventListener('submit', addItem);
-// Delete event
-itemList.addEventListener('click', removeItem);
-// Filter event
-filter.addEventListener('keyup', filterItems);
+function sbmt() {
+    let newLI = document.createElement('li');//creates new list item
+    newLI.tagName = "LI";
+    let newBtn = document.createElement('span');//creates a button to go into new list item
+    newBtn.innerHTML = (myInput).value;//pulls input form into the button text
+    newBtn.className = "itemButton btn-sm";//gives button appropriate class names
+    newBtn.setAttribute("onclick", "deleteItem(event)");
+    newLI.appendChild(newBtn);//puts newBtn into the list item
+    parentUL.appendChild(newLI);//inserts complete button into the origninal UL
+    document.getElementById("myInput").value = ""
+    // console.log(newLI)
+};
 
-// Add item
-function addItem(e){
-  e.preventDefault();
+// / deleteB.addEventListener('click', function(event) {
+// 	console.log("hi")
 
-  // Get input value
-  let newItem = document.getElementById('item').value;
+function deleteItem() {
+    let d86 = event.target;
+    let dPN86 = event.target.parentNode;
+    
+   
+    parentUL.removeChild(dPN86);}
+    //     document.e.target.remove();
 
-  // Create new li element
-  let li = document.createElement('li');
-  // Add class
-  li.className = 'list-group-item';
-  // Add text node with input value
-  li.appendChild(document.createTextNode(newItem));
+    
+//     let children = Array.from(parentUL.children);
+//     children.forEach(function(child){
+//     console.log(child);
+//     //parentUL(remove)
 
-  // Create del button element
-  let deleteBtn = document.createElement('button');
+	// if something, break;
+    // });
 
-  // Add classes to del button
-  deleteBtn.className = 'btn btn-danger btn-sm float-right delete';
+//     function deleteItem(e) {
+//         document.e.target.remove();
+// }
+//     let li = e.target.parentUL;
+//                     console.log(li);
+//                     parentUL.removeChild(li);}
 
-  // Append text node
-  deleteBtn.appendChild(document.createTextNode('X'));
 
-  // Append button to li
-  li.appendChild(deleteBtn);
 
-  // Append li to list
-  itemList.appendChild(li);
-}
 
-// Remove item
-function removeItem(e){
-  if(e.target.classList.contains('delete')){
-    if(confirm('Are You Sure?')){
-      let li = e.target.parentElement;
-      itemList.removeChild(li);
-    }
-  }
-}
+// submit.addEventListener('click', function(event) {
+// 	let newLI = document.createElement('li');
+//     newLI.innerHTML=document.getElementById('newToDo').value;
+//     parentUL.appendChild(newLI);
+//     document.getElementById("newToDo").value = ""});
 
-// Filter Items
-function filterItems(e){
-  // convert text to lowercase
-  let text = e.target.value.toLowerCase();
-  // Get lis
-  let items = itemList.getElementsByTagName('li');
-  // Convert to an array
-  Array.from(items).forEach(function(item){
-    let itemName = item.firstChild.textContent;
-    if(itemName.toLowerCase().indexOf(text) != -1){
-      item.style.display = 'block';
-    } else {
-      item.style.display = 'none';
-    }
-  });
-}
+    
+// deleteB.addEventListener('click', function(event) {
+// 	console.log("hi")
+
+//     let children = Array.from(parentUL.children);
+//     children.forEach(function(child){
+//     console.log(child);
+//     //parentUL(remove)
+
+	// if something, break;
+    // });
+
+
+    // });
+
+
+            // //Calls the UL into memory
+            // const parentUL = document.getElementById('tdUl');
+
+            // //Function executed on click of add button
+            // function submit() {
+            //     let newLI = document.createElement('li');//creates new list item
+            //     let newBtn = document.createElement('button');//creates a button to go into new list item
+            //     newBtn.innerHTML=(myInput).value;//pulls input form into the button text
+            //     newBtn.className="itemButton btn-lg";//gives button appropriate class names
+            //     newLI.appendChild(newBtn);//puts newBtn into the list item
+            //    parentUL.appendChild(newLI);//inserts complete button into the origninal UL
+            //    document.getElementById("myInput").value = ""};//clears the input form
+
+
+            //Calls the UL into memory
+            // let nodeList=document.getElementsByTagName("LI")
+            
+            // let x=0;
+
+            //Function executed on click of add button
+          //clears the input form
+
+            // StrikeThrough
+
+
+
+
+
+
+// // Create a "close" button and append it to each list item
+// var myNodelist = document.getElementsByTagName("LI");
+// var i;
+// for (i = 0; i < myNodelist.length; i++) {
+//   var span = document.createElement("SPAN");
+//   var txt = document.createTextNode("x");
+//   span.className = "close";
+//   span.appendChild(txt);
+//   myNodelist[i].appendChild(span);
+// }
+
+// // Click on a close button to hide the current list item
+// var close = document.getElementsByClassName("close");
+// var i;
+// for (i = 0; i < close.length; i++) {
+//   close[i].onclick = function() {
+//     var div = this.parentElement;
+//     div.style.display = "none";
+//   }
+// }
+
+// // Add a "checked" symbol when clicking on a list item
+// var list = document.querySelector('ul');
+// list.addEventListener('click', function(ev) {
+//   if (ev.target.tagName === 'LI') {
+//     ev.target.classList.toggle('checked');
+//   }
+// }, false);
+
+// // Create a new list item when clicking on the "Add" button
+// function newElement() {
+//   var li = document.createElement("li");
+//   var inputValue = document.getElementById("myInput").value;
+//   var t = document.createTextNode(inputValue);
+//   li.appendChild(t);
+//   if (inputValue === '') {
+//     alert("You must write something!");
+//   } else {
+//     document.getElementById("myUL").appendChild(li);
+//   }
+//   document.getElementById("myInput").value = "";
+
+//   var span = document.createElement("SPAN");
+//   var txt = document.createTextNode("\u00D7");
+//   span.className = "close";
+//   span.appendChild(txt);
+//   li.appendChild(span);
+
+//   for (i = 0; i < close.length; i++) {
+//     close[i].onclick = function() {
+//       var div = this.parentElement;
+//       div.style.display = "none";
+//     }
+//   }
+// }
+
+
+
+//             //    let p = document.getElementById("para1")
+//             //    let p_prime = p.cloneNode(true)
+
+
+
+
+// //             //    Deleting a list item when clicked.
+// // 
+
+
+// //                let listItems = document.getElementsByClassName(itemButton);
+
+// //                dltItem.addEventListener('click', function(event) {
+// //                   	slctd.document.getElementBy
+// //                   //     newLI.innerHTML=document.getElementById('newToDo').value;
+// //                   //     parentUL.appendChild(newLI);
+// //                   //     document.getElementById("newToDo").value = ""});
+
+// // //          const parentUL = document.querySelector('ul');
+// // // const sumbit = document.querySelector('button');
+
+// // // submit.addEventListener('click', function(event) {
+// // // 	let newLI = document.createElement('li');
+// // //     newLI.innerHTML=document.getElementById('newToDo').value;
+// // //     parentUL.appendChild(newLI);
+// // //     document.getElementById("newToDo").value = ""});
+// //  console.log(newLI);
