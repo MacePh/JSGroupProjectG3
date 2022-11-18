5077c36ef47844ce964fa6ff908863614f86ca0b
ClairePNov <cep1124@gmail.com>
2022-11-18 12:31:36 -0500

    delete works

--- Claireform.js
+++ Claireform.js
@@ -2,41 +2,26 @@
 let form = document.getElementById("addForm")
     let list = document.getElementById('itemsUL');
 
-
     // add-task button function
     function getLis() {
         return Array.from(list.childNodes).filter(item => item.tagName === 'LI')
     }
 
     function addTodo(el) {
-        let newLI = document.createElement('li');
+        let newLI = document.createElement('li');//Creates list item
         newLI.id = el.id
-        let newBtn= document.createElement('button')
-        //let txt = document.createTextNode("\u00D7");
-        newBtn.className = "btn btn-secondary btn-sm float-right delete";
-        //newBtn.txt.innerHTML = text
-        //appendChild(txt);
-        //newBtn = newBtn.value;
-      //  newLI.appendChild(newBtn); 
-          newlist.innerHTML = newBtn
         newLI.className = "list-group-item";
         newLI.innerHTML = document.getElementById('item').value;
         list.appendChild(newLI);
+        let newBtn = document.createElement('button');//creates a button to go into new list item
+        newBtn.innerHTML = 'x';//.value;//pulls input form into the button text
+        newBtn.className = "btn btn-secondary btn-sm float-right delete";//gives button appropriate class names
+        newBtn.setAttribute("onclick", "foo(event)");
+        newLI.appendChild(newBtn);//puts newBtn into the list item
+        list.appendChild(newLI);//inserts complete button into the origninal UL
         document.getElementById("item").value = ""
-        console.log(newLI);
+    console.log(newBtn)
     }
-        // //function submitunction sbmt() {
-        //     let newLI = document.createElement('li');//creates new list item
-        //     newLI.tagName = "LI";
-        //     let newBtn = document.createElement('span');//creates a button to go into new list item
-        //     newBtn.innerHTML = (myInput).value;//pulls input form into the button text
-        //     newBtn.className = "itemButton btn-sm";//gives button appropriate class names
-        //     newBtn.setAttribute("onclick", "deleteItem(event)");
-        //     newLI.appendChild(newBtn);//puts newBtn into the list item
-        //     parentUL.appendChild(newLI);//inserts complete button into the origninal UL
-        //     document.getElementById("myInput").value = ""
-        //     // console.log(newLI)
-        // };
 
     form.addEventListener("submit", function (event) {
         event.preventDefault()
@@ -58,27 +43,11 @@ let form = document.getElementById("addForm")
     });
 
 
-
-    // function deleteItem() {
-    //     let d86 = event.target;
-    //     let dPN86 = event.target.parentNode;
-    //     parentUL.removeChild(dPN86);}
-    //     //     document.e.target.remove
-    
-    
-    //     //function submitunction sbmt() {
-    //         let newLI = document.createElement('li');//creates new list item
-    //         newLI.tagName = "LI";
-    //         let newBtn = document.createElement('span');//creates a button to go into new list item
-    //         newBtn.innerHTML = (myInput).value;//pulls input form into the button text
-    //         newBtn.className = "itemButton btn-sm";//gives button appropriate class names
-    //         newBtn.setAttribute("onclick", "deleteItem(event)");
-    //         newLI.appendChild(newBtn);//puts newBtn into the list item
-    //         parentUL.appendChild(newLI);//inserts complete button into the origninal UL
-    //         document.getElementById("myInput").value = ""
-    //         // console.log(newLI)
-    //    // };
-    
+    function foo() {
+        // let t = event.target;
+        let xGonnnGiveItToYa = event.target.parentNode;
+        list.removeChild(xGonnnGiveItToYa);
+    }
 
     // myUL.addEventListener('click', function (event) { // // get the element
 
